ce0e478f2cd94511d219ddbe8cf81adabea8fb57
backend_trongphan <anhmaypro09880@gmail.com>
2023-05-02 06:16:41 +0700

    finished create warehouse sessions

--- src/containers/Pharmacist/Modal/ImportTable.js
+++ src/containers/Pharmacist/Modal/ImportTable.js
@@ -1,22 +1,50 @@
-import React, { useState } from 'react';
-import { Table, Input, Button, Popconfirm, Form } from 'antd';
+import React, { useState,useEffect } from 'react';
+import { Table, Input, Button, Popconfirm, Form,Select, DatePicker } from 'antd';
+import axios from 'axios';
+import moment from 'moment';
+import ImportWarehouse from './ImportWarehouseModal';
 
 const ImportTable = () => {
   const [form] = Form.useForm();
+
+  const [medicineData, setMedicineData] = useState([]);
+
+  const [searchMedicine, setSearchMedicine] = useState("");
+
+  const [medicineId, setMedicineId] = useState();
+
+  const [expiryDate, setExpiryDate] = useState();
+
+  const [reset, setReset] = useState(false);
+
+  const [quantity, setQuantity] = useState(0);
+
   const [data, setData] = useState([
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
   ]);
+
+  useEffect(() => {
+    const fetchCategoryData = async () => {
+      const response = await axios.get(`http://localhost:9000/api/medicines/`, {
+            params:{
+              category_id: -1,
+              medicine_id: -1,
+              key_search: searchMedicine,
+              status: 1,
+              sort_by: 0
+            }
+        });
+        setMedicineData(response.data.data);
+      };
+      fetchCategoryData();
+  }, [searchMedicine]);
+
+  const handleSelectChangeName = (value) => {
+    setSearchMedicine(`${value}`)
+    const selectedMedicine = medicineData.find((medicine) => medicine.name === `${value}`);
+    setMedicineId(selectedMedicine ? selectedMedicine.id: -1);
+  }
+
+  
 
   const onEdit = (record) => {
     form.setFieldsValue(record);
@@ -44,22 +72,35 @@ const ImportTable = () => {
 
   const columns = [
     {
-      title: 'Name',
+      title: 'STT',
+      dataIndex: 'key',
+      editable: true,
+      width: 20,
+    },
+    {
+      title: 'Tên thuốc',
       dataIndex: 'name',
       editable: true,
+      width: 250,
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
+      title: 'Số lượng',
+      dataIndex: 'quantity',
+      width: 100,
+    },
+    {
+      title: 'Đơn vị',
+      dataIndex: 'storageUnit',
+      editable: true,
+      width: 100,
+    },
+    {
+      title: 'Hạn sử dụng',
+      dataIndex: 'expiryDate',
       editable: true,
     },
     {
-      title: 'Address',
-      dataIndex: 'address',
-      editable: true,
-    },
-    {
-      title: 'Action',
+      title: 'Hành động',
       dataIndex: 'operation',
       render: (_, record) =>
         data.length >= 1 ? (
@@ -80,39 +121,92 @@ const ImportTable = () => {
     },
   ];
 
+
+
   const onFinish = (values) => {
-    const { name, age, address } = values;
-    const newData = {
-      key: (data.length + 1).toString(),
+    const { name, storageUnit } = values;
+    const newProduct = {
       name,
-      age,
-      address,
+      medicineId,
+      quantity,
+      storageUnit,
+      expiryDate,
     };
-    setData([...data, newData]);
+
+    // Kiểm tra xem sản phẩm đã tồn tại trong danh sách hay chưa
+    const existingProductIndex = data.findIndex((product) => product.name === name);
+
+    if (existingProductIndex !== -1) {
+      // Sản phẩm đã tồn tại trong danh sách, cộng thêm quantity cho sản phẩm này
+      const existingProduct = data[existingProductIndex];
+      const updatedProduct = {
+        ...existingProduct,
+        quantity: parseFloat(parseFloat(existingProduct.quantity) + parseFloat(quantity)),
+      };
+      setData([
+        ...data.slice(0, existingProductIndex),
+        updatedProduct,
+        ...data.slice(existingProductIndex + 1),
+      ]);
+    } else {
+      // Sản phẩm chưa tồn tại trong danh sách, thêm sản phẩm mới vào danh sách
+      setData([...data, { ...newProduct, key: data.length + 1 }]);
+    }
+
+    setSearchMedicine("");
+    setExpiryDate(null);
     form.resetFields();
+    
+   
+  };
+
+  console.log(data);
+
+  const handleSelectChangeExpiryDate = (date, dateString) => {
+      setExpiryDate(dateString);  
+  }
+
+  const handleChangeQuantity = (event) => {
+    const target = event.target;
+    const value = target.value;
+    setQuantity(value)
+  };
+
+  const handleReset = () => {
+    setReset(true);
+    setTimeout(() => {
+      setReset(false);
+    }, 0);
+   
   };
 
   return (
     <div>
       <Form form={form} onFinish={onFinish} style ={{display: 'flex'}}>
         <Form.Item name="name" style={{ width:'30%', backgroundColor:''}}  rules={[{ required: true }]}>
-          <Input placeholder="Name" />
+          <Select onChange={handleSelectChangeName}  showSearch className='input name' >
+              {medicineData.map(option => (
+                  <option key={option.id} value={option.name}>
+                    {option.name}
+                  </option>
+              ))}
+          </Select>
         </Form.Item>
-        <Form.Item name="quantity" style={{ width:'15%',marginLeft:'2%'}} rules={[{ required: true }]}>
-          <Input placeholder="Số lượng" />
+        <Form.Item name="quantity" style={{ width:'15%',marginLeft:'2%', marginTop:"5px"}} rules={[{ required: true }]}>
+          <Input  type="number" name ="quantity" onChange={handleChangeQuantity} placeholder="Số lượng" />
         </Form.Item>
-        <Form.Item name="storageUnit"  style={{ width:'15%',marginLeft:'2%'}} rules={[{ required: true }]}>
+        <Form.Item name="storageUnit"  style={{ width:'15%',marginLeft:'2%', marginTop:"5px"}} rules={[{ required: true }]}>
           <Input placeholder="Đơn vị" />
         </Form.Item>
-        <Form.Item name="expiryDate"  style={{ width:'15%',marginLeft:'2%'}} rules={[{ required: true }]}>
-          <Input placeholder="Hạn sử dụng" />
-        </Form.Item>
-        <Button style={{ width:'15%',marginLeft:'5%'}} type="primary" htmlType="submit">
-          Add
+        {/* <Form.Item  name="expiryDate"  style={{ width:'15%',marginLeft:'2%', marginTop:"5px"}} rules={[{ required: true }]}> */}
+          <DatePicker defaultValue={reset ? null : moment()} onChange={handleSelectChangeExpiryDate}  style={{ height:"32px",width:'15%',marginLeft:'2%', marginTop:"5px"}} placeholder="Hạn sử dụng" > </DatePicker>
+          {/* <Input placeholder="Hạn sử dụng" /> */}
+        {/* </Form.Item> */}
+        <Button ononClick={handleReset}  style={{ width:'15%',marginLeft:'5%', marginTop:"5px"}} type="primary" htmlType="submit">
+          Thêm thuốc
         </Button>
        
       </Form>
-      
       <br />
       <Table
         bordered
--- src/containers/Pharmacist/Modal/ImportWarehouseModal.js
+++ src/containers/Pharmacist/Modal/ImportWarehouseModal.js
@@ -1,66 +1,237 @@
-import React, { useState } from 'react';
-import { Button, Input, Modal,Table } from 'antd';
+import React, { useState,useEffect } from 'react';
+import { Button, Input, Modal,Table,Select, Form, DatePicker, Popconfirm } from 'antd';
 import './ImportWarehouseModal.scss'
 import { Link } from 'react-router-dom';
 import ImportTable from './ImportTable';
+import axios from "axios";
+import moment from 'moment';
 
 const ImportWarehouse = () =>{
+
    const [open, setOpen] = useState(false);
 
-   const dataSource = [
-      {
-        key: '1',
-        stt: '1',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        actions: (
-          <span>
-            <Link to="/medicine/detail"> 
-                <Button style={{backgroundColor:'#3c8dbc', color: 'white', fontSize: '15px'}}
-                >
-                  Chi tiết</Button>
-            </Link>
-            <Button style={{backgroundColor:'#00a65a', color: 'white', fontSize: '15px'}}>Lịch sử</Button>
-            <Button style={{backgroundColor:'red', color: 'white', fontSize: '15px'}}>Xóa</Button>
-          </span>
-        ),
-       
-      },
-    ];
+   const [searchEmployee, setSearchEmployee] = useState("");
 
-    const columns = [
-      {
-        title: 'STT',
-        dataIndex: 'stt',
-        key: 'stt',
-        editable: true,
-      },
-      {
-        title: 'Tên thuốc',
-        dataIndex: 'name',
-        key: 'name',
-        editable: true,
-      },
-      {
-        title: 'Số lượng',
-        dataIndex: 'age',
-        key: 'age',
-        editable: true,
-      },
-      {
-        title: 'Đơn vị lưu kho',
-        dataIndex: 'address',
-        key: 'address',
-        editable: true,
-      },
-      {
-        title: 'Hạn sử dụng',
-        dataIndex: 'address',
-        key: 'address',
-        editable: true,
-      }
-      
-    ];
+   const [employeeData, setEmployeeData] = useState([]);
+
+   const [employeeId, setEmployeeId] = useState([]);
+
+   const [note, setNote] = useState("");
+
+   useEffect(() => {
+    const fetchCategoryData = async () => {
+      const response = await axios.get(`http://localhost:9000/api/employees/`, {
+            params:{
+              employee_id: -1,
+              key_search: searchEmployee,
+              is_login: -1,
+              is_active: 1
+            }
+        });
+        setEmployeeData(response.data.data);
+      };
+      fetchCategoryData();
+  }, [searchEmployee]);
+
+  const handleSelectChangeNameEmployee = (value) => {
+    setSearchEmployee(`${value}`)
+    const selectedEmployee = employeeData.find((employee) => employee.name === `${value}`);
+    setEmployeeId(selectedEmployee ? selectedEmployee.id: 1);
+  }
+
+  const handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    setNote(value);
+  };
+
+  const handleImportMedicine = async () =>{
+    const response = await axios.post(`http://localhost:9000/api/medicines/create-warehouse`, {
+        employee_id: employeeId,
+        discount_percent: 0,
+        type: 0,
+        discount_amount: 0,
+        description : note,
+        warehouse_session_request:  data
+    });
+    console.log(response);
+
+  }
+
+  const [form] = Form.useForm();
+
+  const [medicineData, setMedicineData] = useState([]);
+
+  const [searchMedicine, setSearchMedicine] = useState("");
+
+  const [medicine_id, setMedicineId] = useState();
+
+  const [expiry_date, setExpiryDate] = useState();
+
+  const [reset, setReset] = useState(false);
+
+  const [quantity, setQuantity] = useState(0);
+
+  const [data, setData] = useState([
+  ]);
+
+  useEffect(() => {
+    const fetchCategoryData = async () => {
+      const response = await axios.get(`http://localhost:9000/api/medicines/`, {
+            params:{
+              category_id: -1,
+              medicine_id: -1,
+              key_search: searchMedicine,
+              status: 1,
+              sort_by: 0
+            }
+        });
+        setMedicineData(response.data.data);
+      };
+      fetchCategoryData();
+  }, [searchMedicine]);
+
+  const handleSelectChangeName = (value) => {
+    setSearchMedicine(`${value}`)
+    const selectedMedicine = medicineData.find((medicine) => medicine.name === `${value}`);
+    setMedicineId(selectedMedicine ? selectedMedicine.id: -1);
+  }
+
+  
+
+  const onEdit = (record) => {
+    form.setFieldsValue(record);
+  };
+
+  const onDelete = (record) => {
+    setData(data.filter((item) => item.key !== record.key));
+  };
+
+  const onSave = (values) => {
+    const newData = [...data];
+    const index = newData.findIndex((item) => values.key === item.key);
+
+    if (index > -1) {
+      const item = newData[index];
+      newData.splice(index, 1, { ...item, ...values });
+      setData(newData);
+      form.resetFields();
+    } else {
+      newData.push(values);
+      setData(newData);
+      form.resetFields();
+    }
+  };
+
+  const columns = [
+    {
+      title: 'STT',
+      dataIndex: 'key',
+      editable: true,
+      width: 20,
+    },
+    {
+      title: 'Tên thuốc',
+      dataIndex: 'name',
+      editable: true,
+      width: 250,
+    },
+    {
+      title: 'Số lượng',
+      dataIndex: 'quantity',
+      width: 100,
+    },
+    {
+      title: 'Đơn vị',
+      dataIndex: 'unit',
+      editable: true,
+      width: 100,
+    },
+    {
+      title: 'Hạn sử dụng',
+      dataIndex: 'expiry_date',
+      editable: true,
+    },
+    {
+      title: 'Hành động',
+      dataIndex: 'operation',
+      render: (_, record) =>
+        data.length >= 1 ? (
+          <div>
+            <Button type="primary" onClick={() => onEdit(record)}>
+              Sửa
+            </Button>
+            <Popconfirm
+              title="Sure to delete?"
+              onConfirm={() => onDelete(record)}
+            >
+              <Button type="danger" style={{ marginLeft: 10 }}>
+                Xóa
+              </Button>
+            </Popconfirm>
+          </div>
+        ) : null,
+    },
+  ];
+
+
+
+  const onFinish = (values) => {
+    const { name, unit } = values;
+    const newProduct = {
+      name,
+      medicine_id,
+      quantity,
+      unit,
+      expiry_date,
+    };
+
+    // Kiểm tra xem sản phẩm đã tồn tại trong danh sách hay chưa
+    const existingProductIndex = data.findIndex((product) => product.name === name);
+
+    if (existingProductIndex !== -1) {
+      // Sản phẩm đã tồn tại trong danh sách, cộng thêm quantity cho sản phẩm này
+      const existingProduct = data[existingProductIndex];
+      const updatedProduct = {
+        ...existingProduct,
+        quantity: parseFloat(parseFloat(existingProduct.quantity) + parseFloat(quantity)),
+      };
+      setData([
+        ...data.slice(0, existingProductIndex),
+        updatedProduct,
+        ...data.slice(existingProductIndex + 1),
+      ]);
+    } else {
+      // Sản phẩm chưa tồn tại trong danh sách, thêm sản phẩm mới vào danh sách
+      setData([...data, { ...newProduct, key: data.length + 1 }]);
+    }
+
+    setSearchMedicine("");
+    setExpiryDate(null);
+    form.resetFields();
+    
+   
+  };
+
+
+  const handleSelectChangeExpiryDate = (date, dateString) => {
+      setExpiryDate(dateString);  
+  }
+
+  const handleChangeQuantity = (event) => {
+    const target = event.target;
+    const value = target.value;
+    setQuantity(value)
+  };
+
+  const handleReset = () => {
+    setReset(true);
+    setTimeout(() => {
+      setReset(false);
+    }, 0);
+   
+  };
 
   return (
     <>
@@ -77,28 +248,70 @@ const ImportWarehouse = () =>{
             >
                <div className='import-warehouse-container'>
                   <div className='btn-add-drug'>
-                        <Button className="btn btn-import" type="primary" >
-
+                        <Button onClick={handleImportMedicine} className="btn btn-import" type="primary" >
+                            Thêm phiếu 
                         </Button>
                   </div>
                   <div className='search-container'>
+                    
                         <div style={{width:'38%'}} >
                            <label style={{marginBottom:'5px'}}>Tên nhân viên</label>
-                           <Input className='input name' ></Input>
+                           <Select onChange={handleSelectChangeNameEmployee} style={{width:"320px" , height:"36px"}} placeholder="Tên nhân viên" showSearch className='input name' >
+                           {employeeData.map(option => (
+                                <option key={option.id} value={option.name}>
+                                  {option.name}
+                                </option>
+                            ))}
+                           </Select>
+                           {/* <Input className='input note'></Input> */}
                         </div>
                         <div style={{width:'60%',marginLeft:'2%'}} >
                            <label style={{marginBottom:'5px'}}>Ghi chú</label>
-                           <Input className='input note'></Input>
+                           <Input placeholder="Ghi chú"  onChange={handleInputChange} className='input note'></Input>
                         </div>
-                  </div>
-                  <div className='table-container'>
-                        <div className='' style={{marginBottom: 16}}>
-                          
-                            
-                        </div>
-                        <h5>Thêm thông tin thuốc</h5>
-                        <ImportTable/>
-                  </div>
+                      </div>
+                   
+                      <div className='table-container'>
+                            <div className='' style={{marginBottom: 16}}>
+                              
+                                
+                            </div>
+                            <h5>Thêm thông tin thuốc</h5>
+                            <div>
+                  <Form form={form} onFinish={onFinish} style ={{display: 'flex'}}>
+                    <Form.Item name="name" style={{ width:'30%', backgroundColor:''}}  rules={[{ required: true }]}>
+                      <Select onChange={handleSelectChangeName}  showSearch className='input name' >
+                          {medicineData.map(option => (
+                              <option key={option.id} value={option.name}>
+                                {option.name}
+                              </option>
+                          ))}
+                      </Select>
+                    </Form.Item>
+                    <Form.Item name="quantity" style={{ width:'15%',marginLeft:'2%', marginTop:"5px"}} rules={[{ required: true }]}>
+                      <Input  type="number" name ="quantity" onChange={handleChangeQuantity} placeholder="Số lượng" />
+                    </Form.Item>
+                    <Form.Item name="unit"  style={{ width:'15%',marginLeft:'2%', marginTop:"5px"}} rules={[{ required: true }]}>
+                      <Input placeholder="Đơn vị" />
+                    </Form.Item>
+                    {/* <Form.Item  name="expiryDate"  style={{ width:'15%',marginLeft:'2%', marginTop:"5px"}} rules={[{ required: true }]}> */}
+                      <DatePicker defaultValue={reset ? null : moment()} onChange={handleSelectChangeExpiryDate}  style={{ height:"32px",width:'15%',marginLeft:'2%', marginTop:"5px"}} placeholder="Hạn sử dụng" > </DatePicker>
+                      {/* <Input placeholder="Hạn sử dụng" /> */}
+                    {/* </Form.Item> */}
+                    <Button ononClick={handleReset}  style={{ width:'15%',marginLeft:'5%', marginTop:"5px"}} type="primary" htmlType="submit">
+                      Thêm thuốc
+                    </Button>
+                  
+                  </Form>
+                  <br />
+                  <Table
+                    bordered
+                    dataSource={data}
+                    columns={columns}
+                    pagination={false}
+                  />
+                </div>
+                              </div>
                </div>
       </Modal>
     </>
