0f99beb4cb4a877405f187f18f1684770c27bf3c
backend_trongphan <anhmaypro09880@gmail.com>
2023-05-23 00:03:24 +0700

    no message

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,10 +21,7 @@ import MedicineDetail from './Pharmacist/MedicineDetail/MedicineDetail';
 import MedicineHistory from './Pharmacist/MedicineHistory/MedicineHistory';
 import MedicineWarning from './Pharmacist/MedicineWarning/MedicineWarning';
 import AllMedicinesHistory from './Pharmacist/AllMedicineHistory/AllMedicine';
-<<<<<<< HEAD
 
-=======
->>>>>>> trongphan
 import Report from './Report/Report';
 import ReportRevenueCostProfit from './Report/ReportRevenueCostProfit';
 import ReportPatientList from './Report/ReportPatientList';
@@ -34,11 +31,6 @@ import ReportExportInventory from './Report/ReportExportInventory';
 import AppProvider from './Pharmacist/Warehouse/AppContext';
 import Warehouse from './Pharmacist/Warehouse/Warehouse';
 
-
-<<<<<<< HEAD
-
-=======
->>>>>>> trongphan
 
 class App extends Component {
 
@@ -69,27 +61,6 @@ class App extends Component {
                     
                             {this.props.isLoggedIn && <Header />}
 
-<<<<<<< HEAD
-                        <span className="content-container">
-                            <Switch>
-                                <Route path={path.HOME} exact component={(Home)} />
-                                <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
-                                <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
-                                <Route path={path.PHARMACIST} component={(Pharmacist)} />
-                                <Route path="/medicine/detail/:id" component={(MedicineDetail)} />
-                                <Route path="/medicine/:id/history" component={(MedicineHistory)} />
-                                <Route path="/medicine/warning" component={(MedicineWarning)} />
-
-                                <Route path="/medicine/history" component={(AllMedicinesHistory)} />
-                                <Route path="/report" component={(Report)} />
-                                <Route path="/report-revenue-cost-profit" component={(ReportRevenueCostProfit)} />
-                                <Route path="/report-patient-list" component={(ReportPatientList)} />
-                                <Route path="/report-export" component={(ReportExport)} />
-                                <Route path="/report-inventory" component={(ReportInventory)} />
-                                <Route path="/report-export-inventory" component={(ReportExportInventory)} />
-                            </Switch>
-                        </span>
-=======
                             <span className="content-container">
                                 <Switch>
                                     <Route path={path.HOME} exact component={(Home)} />
@@ -99,7 +70,6 @@ class App extends Component {
                                     <Route path="/medicine/detail/:id" component={(MedicineDetail)} />
                                     <Route path="/medicine/:id/history" component={(MedicineHistory)} />
                                     <Route path="/medicine/warning" component={(MedicineWarning)} />
->>>>>>> trongphan
 
                                     <Route path="/medicine/history" component={(AllMedicinesHistory)} />
                                     <Route path="/report" component={(Report)} />
